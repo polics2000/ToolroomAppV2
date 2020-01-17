31f66ab838e914664abac6bf62853bbd8fe925b1
Yeonghun Ahn <polics2000@gmail.com>
2020-01-17 19:58:31 +0000

    CRUD part 1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
 var seedDB = require("./seeds");
@@ -17,6 +18,7 @@ mongoose.connect("mongodb://localhost/toolroom");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 seedDB();
 
 // PASSPORT CONFIGURATION
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -53,12 +53,65 @@ router.get("/:id", function(req,res){
 	});
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", checkCampgroundOwnership, function(req,res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		res.render("campgrounds/edit", {campground: foundCampground});
+	});
+});
+
+router.put("/:id", checkCampgroundOwnership, function(req, res){
+	// find and update the correct campground
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res){
+	res.send("Delete reoute!");
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", checkCampgroundOwnership, function(req,res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
 // MIDDLEWARE
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+	if(req.isAuthenticated()){
+			Campground.findById(req.params.id, function(err, foundCampground){
+				if(err){
+					res.redirect("back");
+				} else {
+					// does user own the campground?
+					if(foundCampground.author.id.equals(req.user._id)) {
+						next();
+					} else {
+						res.redirect("back");
+					}	
+				}
+			});
+		} else {
+			res.redirect("back");
+		}
 }
 
 module.exports = router;
